cdc47fe35d9b82e1a9f2d50bed14ad9d12b13abf
abhishek.dyavarisetty <abhishek.dyavarisetty@wisdmlabs.com>
2019-06-26 21:38:08 +0530

    added functionality for the put/update product

--- src/router/product.js
+++ src/router/product.js
@@ -4,87 +4,6 @@ const express = require("express");
 const productRoute = express();
 const coreFunction = require('../../includes/core.function');
 
-productRoute.get('/products', (request, response) => {
-
-  // console.log(request.body.product_category);
-  // console.log(uniqueArray)
-
-//   ProductModel.aggregate([
-//     {
-//       // $unwind: $category_id
-//     },
-//     {
-//       $lookup: {
-//         from: "categories",
-//         localField: "category_id",
-//         forigenField: "product_category",
-//         as: "category"
-//       }
-//     },
-//     {
-//       $match: { "category": { $ne: [] } }
-//    }
-//   ], (error, docs) => {
-//     console.log(docs);
-//   })
-
-//   ProductModel.find({
-//   })
-//     .then(doc => {
-//       return response.json(doc);
-//     })
-//     .catch(err => {
-//       return response.status(500).json(err);
-//     });
-// });
-// console.log(CategoryModel);
-// CategoryModel.find({}, {category_id: 1, category_name: 1, _id: 0}, (errorCategoryFind, categoryFind) => {
-//   if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
-//     return response.status(400).send('Category Not found');
-//   } else if (errorCategoryFind) {
-//     return response.status(500).send(errorCategoryFind);
-//   } else {
-//     return response.status(200).send(categoryFind);
-//   }
-var category = {};
-var childCategory = [];
-CategoryModel.find({}, {category_id: 1, category_name: 1, category_child: 1, _id: 0}, (errorCategoryFind, categoryFind) => {
-  if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
-    return response.status(400).send('no category present');
-  } else if (errorCategoryFind) {
-    return response.status(500).send(errorCategoryFind);
-  } else {
-    categoryFind.forEach((items, index) => {
-      // let id = items.category_id;
-      category[items.category_id] = items.category_name;
-      childCategory.push(items.category_child);
-      // let childCatedoryIds = categoryFind.category_child;
-      // console.log(childCatedoryIds);
-    });
-    childCategory.forEach((items) => {
-      items.forEach((item, index )=> {
-        items[index] = {category_id: item ,category_name: category[item]};
-        // console.log(category[item]);
-      });
-    });
-    // categoryFind.forEach((item) => {
-    //   console.log(item.category_child);
-    // });
-    // return response.status(200).send(categoryFind);
-  }
-  // console.log(category);
-  // console.log(childCategory);
-  // console.log(categoryFind);
-  response.status(200).send(categoryFind);
-});
-// .then(doc => {
-//     response.json(doc);
-// })
-// .catch(err => {
-//     response.status(500).json(err);
-// })
-});
-
 productRoute.post('/products', (request, response) => {
   if (Object.keys(request.body).length === 0 && request.body.constructor === Object) {
     return response.status(400).send('Request body is missing');
@@ -105,7 +24,42 @@ productRoute.post('/products', (request, response) => {
     addProduct(request, response);
     // return productResponse;
   }
-})
+});
+
+productRoute.get('/products/category/:id', (request, response) => {
+  ProductModel.find({product_category: request.params.id}, {product_category: 1, product_id: 1, product_name: 1, _id: 0}, (errorProductsByCategory, productsByCategory) => {
+    // response.send(productsByCategory);
+    if (!errorProductsByCategory && (!productsByCategory || productsByCategory === 0)) {
+      return response.status(400).send('invalid request');
+    } else if (errorProductsByCategory) {
+      return response.status(500).send(errorProductsByCategory);
+    } else {
+      return response.status(200).send(productsByCategory);
+    }
+  });
+});
+
+productRoute.put('/product', (request, response) => {
+  if (!request.query.id) {
+    return response.status(400).send("Missing URL parameter: id");
+  } else {
+    if (request.body.product_category.length !== 0) {
+      CategoryModel.find({category_id: {$in: request.body.product_category}}, (errorCategoryFind, categoryFind) => {
+        if (!errorCategoryFind && (!categoryFind || categoryFind.length !== request.body.product_category.length)) {
+          return response.status(400).send('invalid category');
+        } else if (errorCategoryFind) {
+          return response.status(500).send(errorCategoryFind);
+        } else {
+          updateProduct(request, response);
+          // return productResponse;
+        }
+      });
+    } else {
+      updateProduct(request, response);
+      // return productResponse;
+    }
+  }
+});
 
 function addProduct (request, response) {
   console.log(request.body);
@@ -125,6 +79,48 @@ function addProduct (request, response) {
     } else {
       return response.status(400).send('Product Already Exist');
     }
+  });
+}
+
+function updateProduct(request, response) {
+  ProductModel.findOne({product_id: request.query.id}, (errorProductIdFind, productIdFind) => {
+    // return console.log(productIdFind.product_id);
+    if (!errorProductIdFind && productIdFind) {
+      if (request.query.id == request.body.product_id) {
+        ProductModel.findOneAndUpdate({product_id: request.query.id}, request.body, {useFindAndModify: false ,new: true}, (errorProductFind, productFind) => {
+          if (!errorProductFind && (!productFind || productFind === 0)) {
+            return response.status(400).send('invalid request');
+          } else if (errorProductFind) {
+            return response.status(500).send(errorProductFind);
+          } else {
+            return response.status(200).send(productFind);
+          }
+        });
+      } else {
+        return response.status(400).send('Product id does not match with the query string');
+      }
+    } else {
+      return response.status(400).send('Product does not exist please adding the product');
+    }
+  //   if (!errorProductIdFind && (productIdFind !== 0 || request.query.id == request.body.product_id)) {
+  //     ProductModel.findOneAndUpdate({product_id: request.query.id}, request.body, {useFindAndModify: false ,new: true}, (errorProductFind, productFind) => {
+  //       if (!errorProductFind && (!productFind || productFind === 0)) {
+  //         return response.status(400).send('invalid request');
+  //       } else if (errorProductFind) {
+  //         return response.status(500).send(errorProductFind);
+  //       } else {
+  //         return response.status(200).send(productFind);
+  //       }
+  //     }); 
+  //   } else if (errorProductIdFind) {
+  //     return response.status(500).send(errorProductIdFind);
+  //   } else {
+  //     if (!productIdFind || productIdFind === 0) {
+  //       return response.status(400).send('Product does not exist please adding the product');
+  //     } else {
+  //       return response.status(400).send('Product Id already exist');
+  //     }
+  //   }
   });
 }
 
