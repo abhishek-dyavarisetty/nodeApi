1def68a7254e95e96ec45db363c51d311909f763
abhishek.dyavarisetty <abhishek.dyavarisetty@wisdmlabs.com>
2019-06-26 15:48:24 +0530

    completed with the get categories

--- src/router/category.js
+++ src/router/category.js
@@ -3,32 +3,64 @@ const express = require('express');
 const categoryRoute = express();
 
 categoryRoute.get('/categories', (request, response) => {
-
-    // CategoryModel.find({category_id: {$all: request.body.category_child}}, {category_id: 1, _id: 0 })
-    // .then(doc => {
-    //     console.log(doc);
-    //     if (!docs || docs.length == 0) {
-    //         return response.status(400).send("invalid child category")
-    //     }
-    // })
-    // .catch(err => {
-    //     return response.status(500).json(err);
-    // })
-
-    CategoryModel.find({
-    })
-    .then(doc => {
-        response.json(doc);
-    })
-    .catch(err => {
-        response.status(500).json(err);
-    })
+    var category = {};
+    var childCategory = [];
+    CategoryModel.find({}, {category_id: 1, category_name: 1, category_child: 1, _id: 0}, (errorCategoryFind, categoryFind) => {
+        if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
+            return response.status(400).send('no category present');
+        } else if (errorCategoryFind) {
+            return response.status(500).send(errorCategoryFind);
+        } else {
+            categoryFind.forEach((items, index) => {
+            // let id = items.category_id;
+            category[items.category_id] = items.category_name;
+            childCategory.push(items.category_child);
+            // let childCatedoryIds = categoryFind.category_child;
+            // console.log(childCatedoryIds);
+            });
+            childCategory.forEach((items) => {
+            items.forEach((item, index )=> {
+                items[index] = {category_id: item ,category_name: category[item]};
+                // console.log(category[item]);
+            });
+            });
+            // categoryFind.forEach((item) => {
+            //   console.log(item.category_child);
+            // });
+            // return response.status(200).send(categoryFind);
+        }
+        // console.log(category);
+        // console.log(childCategory);
+        // console.log(categoryFind);
+        response.status(200).send(categoryFind);
+    });
 });
 
 categoryRoute.post('/categories', (request, response) => {
     if (Object.keys(request.body).length === 0 && request.body.constructor === Object) {
         return response.status(400).send('Request body is missing');
     }
+
+    if (request.body.category_child != 0) {
+        console.log(request.body.category_child);
+        CategoryModel.find({category_id: {$in: request.body.category_child}}, (errorCategoryChildFind, categoryChildFind) => {
+        //     if(categoryChildFind.length !== request.body.category_child.length) {
+        //         return response.status(400).send('child category does not exist');
+        //     }
+            if (!errorCategoryChildFind && (!categoryChildFind || categoryChildFind.length !== request.body.category_child.length)) {
+                return response.status(400).send('invalid child category');
+            } else if (errorCategoryChildFind) {
+                return response.status(500).send(errorCategoryChildFind);
+            } else {
+                addCategory(request, response);
+            }
+        });
+    } else {
+        // console.log(2);
+        addCategory(request, response);
+    }
+
+
 
     // CategoryModel.find({category_id: {$all: request.body.category_child}}, {category_id: 1, _id: 0 })
     // .then(doc => {
@@ -45,17 +77,38 @@ categoryRoute.post('/categories', (request, response) => {
     //     return request.body.category_child.indexOf(item) == pos;
     // })
 
-    let model = new CategoryModel(request.body)
-    model.save()
-    .then(doc => {
-        if (!doc || doc.length == 0) {
-            return response.status(500).send(doc);
-        }
-        response.status(201).json(doc);
-    })
-    .catch(err => {
-        response.status(500).json(err);
-    })
+    // let model = new CategoryModel(request.body)
+    // model.save()
+    // .then(doc => {
+    //     if (!doc || doc.length == 0) {
+    //         return response.status(500).send(doc);
+    //     }
+    //     response.status(201).json(doc);
+    // })
+    // .catch(err => {
+    //     response.status(500).json(err);
+    // })
 })
+
+function addCategory (request, response) {
+    CategoryModel.find({category_id: request.body.category_id}, (errorCategoryFind, categoryFind) => {
+        if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
+            let model = new CategoryModel(request.body)
+            model.save().then(insertCategory => {
+                if (!insertCategory || insertCategory.length === 0) {
+                    return response.status(500).send(insertCategory);
+                }
+                response.status(201).json(insertCategory);
+            })
+            .catch(insertError => {
+                response.status(500).json(insertError);
+            })
+        } else if (errorCategoryFind) {
+            response.status(500).send(errorCategoryFind);
+        } else {
+            response.status(400).send("Category Already Exist");
+        }
+    });
+}
 
 module.exports = categoryRoute;
--- src/router/product.js
+++ src/router/product.js
@@ -7,16 +7,82 @@ const coreFunction = require('../../includes/core.function');
 productRoute.get('/products', (request, response) => {
 
   // console.log(request.body.product_category);
-  console.log(uniqueArray)
+  // console.log(uniqueArray)
 
-  ProductModel.find({
-  })
-    .then(doc => {
-      return response.json(doc);
-    })
-    .catch(err => {
-      return response.status(500).json(err);
+//   ProductModel.aggregate([
+//     {
+//       // $unwind: $category_id
+//     },
+//     {
+//       $lookup: {
+//         from: "categories",
+//         localField: "category_id",
+//         forigenField: "product_category",
+//         as: "category"
+//       }
+//     },
+//     {
+//       $match: { "category": { $ne: [] } }
+//    }
+//   ], (error, docs) => {
+//     console.log(docs);
+//   })
+
+//   ProductModel.find({
+//   })
+//     .then(doc => {
+//       return response.json(doc);
+//     })
+//     .catch(err => {
+//       return response.status(500).json(err);
+//     });
+// });
+// console.log(CategoryModel);
+// CategoryModel.find({}, {category_id: 1, category_name: 1, _id: 0}, (errorCategoryFind, categoryFind) => {
+//   if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
+//     return response.status(400).send('Category Not found');
+//   } else if (errorCategoryFind) {
+//     return response.status(500).send(errorCategoryFind);
+//   } else {
+//     return response.status(200).send(categoryFind);
+//   }
+var category = {};
+var childCategory = [];
+CategoryModel.find({}, {category_id: 1, category_name: 1, category_child: 1, _id: 0}, (errorCategoryFind, categoryFind) => {
+  if (!errorCategoryFind && (!categoryFind || categoryFind.length === 0)) {
+    return response.status(400).send('no category present');
+  } else if (errorCategoryFind) {
+    return response.status(500).send(errorCategoryFind);
+  } else {
+    categoryFind.forEach((items, index) => {
+      // let id = items.category_id;
+      category[items.category_id] = items.category_name;
+      childCategory.push(items.category_child);
+      // let childCatedoryIds = categoryFind.category_child;
+      // console.log(childCatedoryIds);
     });
+    childCategory.forEach((items) => {
+      items.forEach((item, index )=> {
+        items[index] = {category_id: item ,category_name: category[item]};
+        // console.log(category[item]);
+      });
+    });
+    // categoryFind.forEach((item) => {
+    //   console.log(item.category_child);
+    // });
+    // return response.status(200).send(categoryFind);
+  }
+  // console.log(category);
+  // console.log(childCategory);
+  // console.log(categoryFind);
+  response.status(200).send(categoryFind);
+});
+// .then(doc => {
+//     response.json(doc);
+// })
+// .catch(err => {
+//     response.status(500).json(err);
+// })
 });
 
 productRoute.post('/products', (request, response) => {
@@ -24,67 +90,31 @@ productRoute.post('/products', (request, response) => {
     return response.status(400).send('Request body is missing');
   }
 
-  // CategoryModel.find({category_id: {$all: request.body.product_category}}, {category_id: 1, _id: 0 })
-  //   .then(docs => {
-  //     if (!docs || docs.length == 0) {
-  //       return response.status(400).send("invalid category")
-  //     }
-  //   })
-  //   .catch(err => {
-  //     return response.status(500).json(err);
-  //   });
-
-  // let user = {
-  //   name: 'firstname lastname',
-  //   email: 'email@gmail.com'
-  // }
-  // request.body.product_category = request.body.product_category.filter((item, pos) => {
-  //   return request.body.product_category.indexOf(item) == pos;
-  // })
-  // console.log(request.body.product_category.length);
-  if (request.body.product_category.length != 0) {
-    CategoryModel.find({product_category: {$all: request.body.product_category}}, (errorCategoryFind, categoryFind) => {
-      if (!errorCategoryFind && (!categoryFind || categoryFind.length == 0)) {
+  if (request.body.product_category.length !== 0) {
+    CategoryModel.find({category_id: {$in: request.body.product_category}}, (errorCategoryFind, categoryFind) => {
+      if (!errorCategoryFind && (!categoryFind || categoryFind.length !== request.body.product_category)) {
         return response.status(400).send('invalid category');
       } else if (errorCategoryFind) {
         return response.status(500).send(errorCategoryFind);
       } else {
-        console.log(1);
         addProduct(request, response);
         // return productResponse;
       }
     });
   } else {
-    console.log(2);
     addProduct(request, response);
     // return productResponse;
   }
-
-
-  
-  // let model = new ProductModel(request.body)
-  // model.save()
-  //   .then(doc => {
-  //     if (!doc || doc.length === 0) {
-  //       return response.status(500).send(doc)
-  //     }
-
-  //     response.status(201).send(doc)
-  //   })
-  //   .catch(err => {
-  //     response.status(500).json(err)
-  //   })
 })
 
 function addProduct (request, response) {
   console.log(request.body);
   ProductModel.find({product_id: request.body.product_id}, (errorProductFind, productFind) => {
     if (!errorProductFind && (!productFind || productFind.length === 0)) {
-      console.log('if me');
       let model = new ProductModel(request.body);
       model.save().then(insertProduct => {
         if (!insertProduct || insertProduct.length === 0) {
-          return response.status(400).send(insertProduct)
+          return response.status(500).send(insertProduct)
         }
         response.status(201).send(insertProduct);
       }).catch(insertError => {
@@ -93,7 +123,6 @@ function addProduct (request, response) {
     } else if (errorProductFind) {
       return response.status(500).send(errorProductFind);
     } else {
-      console.log('exist');
       return response.status(400).send('Product Already Exist');
     }
   });
